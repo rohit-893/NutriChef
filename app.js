9c66e2a94e1888be4edf32ab711c38ca9feebd1b
Rohit Kumar <rohit0908kr@gmail.com>
2024-11-25 22:31:45 +0530

    added routes

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req,res) =>{
+    res.render('index.ejs')
+})
+
+app.get('/recipe', (req,res) => {
+    res.render('show.ejs')
+})
+
+app.listen(3000, () => {
+    console.log("Listening on Port 3000!")
+})
