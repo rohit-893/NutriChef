b129f5632420eb78737c0164afefba20b24cac20
Rohit Kumar <rohit0908kr@gmail.com>
2024-11-27 21:52:58 +0530

    connected mongo

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/mini-project')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Connection error:', err));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
