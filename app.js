c623eba632e3c3eee4173ab90e342e5a3fe273a9
Rohit Kumar <rohit0908kr@gmail.com>
2024-12-09 23:34:56 +0530

    Revert "Merge branch 'api-implement' of https://github.com/rohit-893/mini-project into api-implement"
    This reverts commit e59cabcc63b176d0d6c72b9a80ba11c0e3d26eef, reversing
    changes made to 9173ed16f2a6117dd64718066683998357bcdf52.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index.ejs');
 })
 
 // app.get('/recipe', (req, res) => {
@@ -20,17 +20,16 @@ app.get('/search', async (req, res) => {
     const page = parseInt(req.query.page) || 1;  // Get the page number from the query parameter (default to 1)
     const limit = 10;  // Number of recipes to show per page
     const ingredient = req.query.ingredient;
-    const response = await axios.get(
-        `https://api.spoonacular.com/recipes/findByIngredients?apiKey=c380b83969ff408698f2a690b3902130&ingredients=${ingredient}&number=50`
-    );
-    const searchResults = response.data;
+    const response = await axios.get(`https://api.spoonacular.com/food/search?apiKey=c380b83969ff408698f2a690b3902130&query=${ingredient}&number=50`);
+    const recipes = response.data;
+    const searchResults = response.data.searchResults[0].results;
     const defaultServings = 4;
 
     const totalRecipes = searchResults.length;
     // Calculate the total number of pages
     const totalPages = Math.ceil(totalRecipes / limit);
 
-    res.render('search', {
+    res.render('search.ejs', {
         searchResults,
         page,
         totalPages,
@@ -44,9 +43,15 @@ app.get('/show/:id?', async (req, res) => {
     const desiredServings = parseInt(req.query.servings) || 4; // Get desired servings or default to 1
     const defaultServings = desiredServings;
     try {
+<<<<<<< HEAD
         const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=c380b83969ff408698f2a690b3902130&nutrition=false`);
         const recipe = response.data;
         console.log(recipe);
+=======
+        const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=c380b83969ff408698f2a690b3902130&includeNutrition=false`);
+        const recipe = response.data;
+
+>>>>>>> parent of e59cabc (Merge branch 'api-implement' of https://github.com/rohit-893/mini-project into api-implement)
         // Adjust ingredient amounts based on desired servings
         const originalServings = recipe.servings;
         const ingredientMap = new Map();
@@ -70,15 +75,16 @@ app.get('/show/:id?', async (req, res) => {
 
         const referer = req.get('Referrer') || '/'; // Get the referrer URL, or fallback to home
 
-        const nutritionLabel = await axios.get("https://api.spoonacular.com/recipes/<%= recipe.id %>/nutritionLabel.png?apiKey=c380b83969ff408698f2a690b3902130");
-
         // Pass the adjusted ingredients and desired servings to the template
+<<<<<<< HEAD
         res.render('show', {
+=======
+        res.render('show.ejs', {
+>>>>>>> parent of e59cabc (Merge branch 'api-implement' of https://github.com/rohit-893/mini-project into api-implement)
             recipe,
             adjustedIngredients,
             referer,
-            defaultServings,
-            nutritionLabel: nutritionLabel.data,
+            defaultServings
         });
     } catch (error) {
         console.error('Error fetching recipe data:', error.message);
