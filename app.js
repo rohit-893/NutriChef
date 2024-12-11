23e6b7108dad26f04b31cc9da2b5a90ddca5e508
preetham92 <preetham92.official@gmail.com>
2024-12-11 20:57:28 +0530

    made changes in cards and search button

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const app = express();
 const path = require("path");
 
-const API_KEY = "01ab36ef25eb40ce993f06e63ae4a30b"; // Replace with your actual Spoonacular API key
+const API_KEY = "928bbe78556741c89c46967b85e5ee17"; // Replace with your actual Spoonacular API key
 
 // Simple in-memory cache (this can be further improved with a library like Redis for production)
 const cache = {};
